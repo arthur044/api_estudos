bb5d3bf1ef9c98d8fa15e3c8e1f4f7f696c42f62
arthur044 <arthuralima.com.br@gmail.com>
2024-08-22 22:05:20 -0300

    coringando

--- src/Controller/imgsController.js
+++ src/Controller/imgsController.js
@@ -1,16 +1,24 @@
 import { Router } from "express";
 const endpoints = Router();
 import multer from "multer";
+import express from 'express'
 let uploadPerfil = multer({ dest: "./storage/imgPerfil" });
+// libera a pasta storage para uso externo
+endpoints.use('./storage/imgPerfil', express.static('./storage/imgPerfil'))
+// nome do parametro que vai pegar a imagem dentro do paranteses do single
 
-endpoints.post("/storage/perfil", uploadPerfil.single("imagem"), (req, resp) => {
-  let caminho = req.file.path;
-  let extensao = req.file.mimetype;
-  let nome = req.file.originalname;
-  resp.send({
-    caminho: caminho,
-    extensao: extensao,
-    nome: nome,
-  });
-});
+endpoints.post(
+  "/storage/perfil",
+  uploadPerfil.single("imagem"),
+  (req, resp) => {
+    let caminho = req.file.path;
+    let extensao = req.file.mimetype;
+    let nome = req.file.originalname;
+    resp.send({
+      caminho: caminho,
+      extensao: extensao,
+      nome: nome,
+    });
+  }
+);
 export default endpoints;
